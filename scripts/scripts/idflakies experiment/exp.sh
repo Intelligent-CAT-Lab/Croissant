149b3cc287374472e56418a1727c14312826286a
LALAYANG <583334298@qq.com>
2022-07-08 17:00:05 +0000

    update exp.sh for logs

--- scripts/scripts/idflakies experiment/exp.sh
+++ scripts/scripts/idflakies experiment/exp.sh
@@ -12,7 +12,7 @@ names=(
 for name in ${names[@]}; do
     export SUBJECT_NAME=$name
     echo $name
-    python3 idflakies_experiment.py 
+    python3 idflakies_experiment.py >>./logs/$currentTimeStamp.log 
 done
 echo ENDING at $(date) >>./logs/$currentTimeStamp.log
 
