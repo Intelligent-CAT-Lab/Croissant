6b220fdd30242e1c1c41b173c0e9da667e8d4b05
LALAYANG <583334298@qq.com>
2022-07-08 16:35:15 +0000

    update exp.sh for logs

--- scripts/scripts/idflakies experiment/exp.sh
+++ scripts/scripts/idflakies experiment/exp.sh
@@ -1,3 +1,10 @@
+current=`date "+%Y-%m-%d %H:%M:%S"`  
+timeStamp=`date -d "$current" +%s`
+currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
+
+mkdir logs
+echo VERSION $(git rev-parse HEAD) >>./logs/$currentTimeStamp.log
+echo STARTING at $(date) >>./logs/$currentTimeStamp.log
 
 names=(
     "svd"
@@ -7,4 +14,5 @@ for name in ${names[@]}; do
     echo $name
     python3 idflakies_experiment.py 
 done
+echo ENDING at $(date) >>./logs/$currentTimeStamp.log
 
