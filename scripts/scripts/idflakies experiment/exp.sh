b75559ffb458b9e71543a25b9ce6ab8a2cf63c33
LALAYANG <583334298@qq.com>
2022-07-08 19:28:08 +0000

    polish exp.sh

--- scripts/scripts/idflakies experiment/exp.sh
+++ scripts/scripts/idflakies experiment/exp.sh
@@ -1,12 +1,14 @@
-current=`date "+%Y-%m-%d %H:%M:%S"`  
-timeStamp=`date -d "$current" +%s`
+current=$(date "+%Y-%m-%d %H:%M:%S")  
+timeStamp=$(date -d "$current" +%s)
 currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
 
-if  [ ! -d  "logs"  ]; then
-mkdir logs
-fi    
-echo VERSION $(git rev-parse HEAD) >>./logs/$currentTimeStamp.log
-echo STARTING at $(date) >>./logs/$currentTimeStamp.log
+mkdir -p logs
+exec 3>&1 4>&2
+trap $(exec 2>&4 1>&3) 0 1 2 3
+exec 1>./logs/$currentTimeStamp.log 2>&1
+
+echo VERSION $(git rev-parse HEAD)
+echo STARTING at $(date)
 
 names=(
     "svd"
@@ -14,7 +16,6 @@ names=(
 for name in ${names[@]}; do
     export SUBJECT_NAME=$name
     echo $name
-    python3 idflakies_experiment.py >>./logs/$currentTimeStamp.log 
+    python3 idflakies_experiment.py
 done
-echo ENDING at $(date) >>./logs/$currentTimeStamp.log
-
+echo ENDING at $(date)
