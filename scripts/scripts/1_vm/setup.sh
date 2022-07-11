d89249a8ce43f5376222486dc66a7e7612599870
LALAYANG <583334298@qq.com>
2022-07-11 22:02:54 +0800

    update setup.sh

--- scripts/scripts/1_vm/setup.sh
+++ scripts/scripts/1_vm/setup.sh
@@ -1,6 +1,23 @@
-
-
 #!/usr/bin/env bash
+current=$(date "+%Y-%m-%d %H:%M:%S")  
+timeStamp=$(date -d "$current" +%s)
+currentTimeStamp=$((timeStamp*1000+$(date "+%N")/1000000)) 
+
+currentPath=$(pwd)
+echo $currentPath
+cd ../../../
+mkdir -p logs
+cd logs
+logPath=$(pwd)
+echo $logPath
+
+cd $currentPath
+exec 3>&1 4>&2
+trap $(exec 2>&4 1>&3) 0 1 2 3
+exec 1>$logPath/setupEnv$currentTimeStamp.log 2>&1
+
+echo VERSION $(git rev-parse HEAD)
+echo STARTING at $(date)
 
 #   setup Java 8
 sudo apt-get update
@@ -21,3 +38,4 @@ sudo apt install python3.8 -y
 sudo dnf install git-all 
 sudo apt install git-all -y
 
+echo ENDING at $(date)
