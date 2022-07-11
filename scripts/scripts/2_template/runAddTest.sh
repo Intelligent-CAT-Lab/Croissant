b790c65598b38311a3073e0f97731332b1e4e9ed
LALAYANG <583334298@qq.com>
2022-07-11 22:05:49 +0800

    add a script for logs

---
+++ scripts/scripts/2_template/runAddTest.sh
@@ -0,0 +1,23 @@
+current=$(date "+%Y-%m-%d %H:%M:%S")  
+timeStamp=$(date -d "$current" +%s)
+currentTimeStamp=$((timeStamp*1000+$(date "+%N")/1000000)) 
+
+currentPath=$(pwd)
+echo $currentPath
+cd ../../../
+mkdir -p logs
+cd logs
+logPath=$(pwd)
+echo $logPath
+
+cd $currentPath
+exec 3>&1 4>&2
+trap $(exec 2>&4 1>&3) 0 1 2 3
+exec 1>$logPath/runAddTest$currentTimeStamp.log 2>&1
+
+echo VERSION $(git rev-parse HEAD)
+echo STARTING at $(date)
+
+python3 add_test.py
+
+echo ENDING at $(date)
